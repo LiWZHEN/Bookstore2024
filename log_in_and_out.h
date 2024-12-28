db30125d25700be9dd89d7d001b62684e1b4aac0
LiWZHEN <liwenzhen@sjtu.edu.cn>
2024-12-28 23:17:13 +0800

    add privilege to the struct

--- log_in_and_out.h
+++ log_in_and_out.h
@@ -1,21 +1,23 @@
 #ifndef LOG_IN_AND_OUT_H
 #define LOG_IN_AND_OUT_H
 
+#include <utility>
+
 #include "user_data.h"
 
 const int stack_capacity = 1000;
 
 struct user_book_privilege {
   std::string username;
-  std::string selected_book;
-  int privilege;
+  std::string selected_book = "null";
+  int privilege = 1;
 };
 
 class stack {
 public:
   stack() = default;
   void LogIn(std::string name, std::string book, int privilege) {
-    log_stack[size] = {name, book, privilege};
+    log_stack[size] = {std::move(name), book, privilege};
     ++size;
   }
   void LogOut() {
